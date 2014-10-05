c22ec399bf7bfb05347972e49baa299fa1c68911
bahartmann <barbarahartma@gmail.com>
2014-10-05 12:31:07 -0300

    user profile authorizations

--- app/controllers/user_profiles_controller.rb
+++ app/controllers/user_profiles_controller.rb
@@ -2,17 +2,27 @@
 class UserProfilesController < ApplicationController
 
   before_action :authenticate_user!, except: [:show]
+  before_action :get_user_profile
+  before_action :authorize_user!, only: [:edit, :update]
+
+  def get_user_profile
+    @user_profile = UserProfile.find(params[:id])
+  end    
+
+  def authorize_user!
+    unless @user_profile.id == current_user.user_profile.id
+      flash[:alert] = "Você não pode editar este perfil."
+      redirect_to user_profile_path(@user_profile)
+    end
+  end
 
   def edit
-    @user_profile = UserProfile.find(params[:id])
   end
 
   def show
-    @user_profile = UserProfile.find(params[:id])
   end
 
   def update
-    @user_profile = UserProfile.find(params[:id])
     if @user_profile.update_attributes(user_profile_params)
       redirect_to @user_profile
     else
@@ -27,5 +37,4 @@ class UserProfilesController < ApplicationController
       .require(:user_profile)
       .permit(:name, :about)
   end
-
 end
