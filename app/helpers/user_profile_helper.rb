56734914da00a7f2a756761877ee0c46e73bcaf5
bahartmann <barbarahartma@gmail.com>
2014-11-10 19:29:33 -0200

    user profile view

--- app/helpers/user_profile_helper.rb
+++ app/helpers/user_profile_helper.rb
@@ -1,6 +1,6 @@
 module UserProfileHelper
   def follow_button is_follower
     label = is_follower ? 'Deixar de Seguir' : 'Seguir'
-    link_to label, follow_user_profile_path(@user_profile), class: "btn btn-default", method: :put
+    link_to label, follow_user_profile_path(@user_profile), class: "btn btn-primary", method: :put
   end
 end
